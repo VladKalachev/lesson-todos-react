23baaa7c7ade5e9cc1d8e339e38c483b10762891
Olena Kopyl <olenatriasii@gmail.com>
2021-11-24 22:13:20 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import ToDo from './ToDo'
-import ToDoForm from './ToDoForm'
+import { useState } from 'react';
+import ToDo from './ToDo';
+import ToDoForm from './ToDoForm';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos ] = useState([])
 
   const addTask = (userInput) => {
-    if(userInput) {
+    if (userInput) {
       const newItem = {
         id: Math.random().toString(36).substr(2,9),
         task: userInput,
@@ -14,19 +14,19 @@ function App() {
       }
       setTodos([...todos, newItem])
     }
-  }
+  };
 
   const removeTask = (id) => {
     setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
+  };
 
   const handleToggle = (id) => {
     setTodos([
-      ...todos.map((todo) => 
-        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
+      ...todos.map((todo) =>
+      todo.id === id ? {...todo, complete: !todo.complete} : {...todo}
       )
     ])
-  }
+  };
 
   return (
     <div className="App">
